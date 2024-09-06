30817ef99db4d26d09dc19f544d6ce3e661b0b21
Idowu Helen <idowuhelenoluwatosin@gmail.com>
2024-09-06 11:54:47 +0100

    initialize repository

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Homecards from "./components/Homecards";
+import JobListings from "./components/JobListings";
+
+const App = () => {
+  return (
+    <>
+    <Hero />
+      <Homecards />
+      <JobListings />
+      
+    </>
+  )
+};
+
+export default App;
